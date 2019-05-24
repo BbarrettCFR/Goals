12ffb079eab1da577b6c3e4e703c1f1b964fc88a
Brandon <47222125+BbarrettCFR@users.noreply.github.com>
2019-05-24 16:29:09 -0400

    component update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import AppHeader from "./AppHeader";
+import DayTracker from "./DayTracker";
 
 function App() {
   return (
     <div className="App">
       <AppHeader />
+      <DayTracker />
     </div>
   );
 }
---
+++ src/DayTracker.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import "./DayTracker.css";
+
+function DayTracker() {
+  const [days, setDays] = useState("1");
+  return (
+    <div class="day-box">
+      <div className="day-counter">
+        <h3>Days You've Been at it:</h3>
+        <h4>{days}</h4>
+      </div>
+    </div>
+  );
+}
+export default DayTracker;
