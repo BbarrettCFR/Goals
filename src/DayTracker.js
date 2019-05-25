a121216fd437c81e591fd2b4e56ad003bfc85a20
Brandon <47222125+BbarrettCFR@users.noreply.github.com>
2019-05-25 11:36:13 -0400

    update state in component

--- src/DayTracker.js
+++ src/DayTracker.js
@@ -2,12 +2,19 @@ import React, { useState } from "react";
 import "./DayTracker.css";
 
 function DayTracker() {
-  const [days, setDays] = useState("1");
+  const [days, setDays] = useState(1);
+  const [hours, setHours] = useState(24);
+
   return (
     <div class="day-box">
       <div className="day-counter">
         <h3>Days You've Been at it:</h3>
         <h4>{days}</h4>
+
+        <h4>Hours:</h4>
+        <p> {hours}</p>
+
+        <button onClick={() => setDays(days + 1)}>+</button>
       </div>
     </div>
   );
